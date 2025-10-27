10001e728daa2ec0cfa92f33d39f3b10d1351add
silveranon323 <101815577+silveranon323@users.noreply.github.com>
2025-10-27 22:31:52 +0530

    first commit

---
+++ src/main/java/com/example/learn_mongodb/LearnMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learn_mongodb/LearnMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
